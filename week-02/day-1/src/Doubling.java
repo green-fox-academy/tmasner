64c50b623481d5f01b3feb53c15692dcbe450277
tmasner <magictonda@gmail.com>
2019-04-08 13:42:43 +0200

    commited execices on functions

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,16 @@
+public class Doubling {
+
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+public static void main(String[] args) {
+
+      int baseNum = 123;
+
+      System.out.println(doubling(baseNum));
+}
+    public static int doubling (int a){
+        int b = a*2;
+        return b;
+    }
+}
