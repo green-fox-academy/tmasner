780c9a92decb2cef824d2be82f2ffa6164db5b80
tmasner <magictonda@gmail.com>
2019-04-09 13:31:40 +0200

    added UrlFixer

---
+++ week-02/day-2/src/UrlFixer.java
@@ -0,0 +1,15 @@
+public class UrlFixer {
+    public static void main(String... args){
+        String Url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crutial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+
+        String newUrl = Url.replaceAll("bots", "odds");
+
+        String NewUrl = newUrl.replaceFirst("https","https:");
+
+        System.out.println(NewUrl);
+    }
+}
