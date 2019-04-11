4921579edf6f82af9e811cee8ee33a460503782c
tmasner <magictonda@gmail.com>
2019-04-11 13:11:11 +0200

    added SumDigits

---
+++ week-02/day-4/src/Power.java
@@ -0,0 +1,7 @@
+public class Power {
+    //Given base and n that are both 1 or more,
+    // compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+    public static void main(String[] args) {
+        
+    }
+}
--- week-02/day-4/src/SumDigits.java
+++ week-02/day-4/src/SumDigits.java
@@ -2,8 +2,18 @@ public class SumDigits {
     //Given a non-negative int n, return the sum of its digits recursively (no loops).
     // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
     // while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    // SumDigits (126 -> 1+2+6=9)
 
     public static void main(String[] args) {
+        int x = 126;
+        System.out.println(SumDigits (x));
 
+    }
+    public static int SumDigits (int n) {
+        if (n == 0){
+            return 0;
+        } else {
+            return n %10 + SumDigits(n / 10);
+        }
     }
 }
