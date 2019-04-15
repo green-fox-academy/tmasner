e9b19f4ef756118f2a2a9de86660bcbcf46bb600
tmasner <magictonda@gmail.com>
2019-04-15 14:35:27 +0200

    added CountLines

--- week-03/day-1/src/CountLines.java
+++ week-03/day-1/src/CountLines.java
@@ -1,7 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
 public class CountLines {
+    public static void main(String[] args) {
+        String nameOfTheFile = "newTextFile.txt";
+        System.out.println(countLines(nameOfTheFile));
+
+
+    }
+
     // Write a function that takes a filename as string,
     // then returns the number of lines the file contains.
     // It should return zero if it can't open the file, and
     // should not raise any error.
-    
+
+    public static int countLines(String nameOfFile) {
+
+        Path filePath = Paths.get("newTextFile.txt");
+
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            return lines.size();
+        } catch (
+                IOException e) {
+            return 0;
+        }
+    }
 }
