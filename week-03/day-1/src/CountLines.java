8d21f5ba584c3b35c5c9f045eaee5679180bbbbb
tmasner <magictonda@gmail.com>
2019-04-15 13:54:44 +0200

    added PrintEachLines

---
+++ week-03/day-1/src/CountLines.java
@@ -0,0 +1,7 @@
+public class CountLines {
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and
+    // should not raise any error.
+    
+}
