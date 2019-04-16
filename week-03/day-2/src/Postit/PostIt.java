e63c20c45e0529402234c0b6d16a30483ad11a13
tmasner <magictonda@gmail.com>
2019-04-16 13:35:33 +0200

    added Postit to week3,day2

---
+++ week-03/day-2/src/Postit/PostIt.java
@@ -0,0 +1,14 @@
+package Postit;
+
+public class PostIt {
+
+    String backgroundCoulor;
+    String text;
+    String textColor;
+
+    public PostIt(String backgroundCoulor, String text, String textColor) {
+        this.backgroundCoulor = backgroundCoulor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
---
+++ week-03/day-2/src/Postit/main.java
@@ -0,0 +1,10 @@
+package Postit;
+
+public class main {
+    public static void main(String[] args) {
+
+        PostIt firstPost = new PostIt("orange", "Idea 1", "blue");
+        PostIt secondPost = new PostIt("pink", "Awesome", "black");
+        PostIt thirdPost = new PostIt("yellow", "Superb!", "green");
+    }
+}
