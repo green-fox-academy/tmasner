e34498ac27870a33df238f8d2fb4e668f27827eb
tmasner <magictonda@gmail.com>
2019-04-22 17:45:58 +0200

    added week-04

---
+++ week-03/day-2/src/Farm/Animal.java
@@ -0,0 +1,17 @@
+package Farm;
+
+public class Animal {
+    int hunger = 50;
+    int thirst = 50;
+
+    public void eat() {
+        hunger--;
+    }
+    public  void drink() {
+        thirst--;
+    }
+    public  void play(){
+        hunger++;
+        thirst--;
+    }
+}
