b76b012d8e6662e1f9619aa51edd26eb53d7b610
tmasner <magictonda@gmail.com>
2019-05-14 17:36:44 +0200

    exercise 7 finished

--- week-06/day-2/src/Exercise7.java
+++ week-06/day-2/src/Exercise7.java
@@ -1,11 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Exercise7 {
     public static void main(String[] args) {
         //Write a Stream Expression to find the strings which starts with a given letter(as parameter), in the following list:
 
+        String startingLetter= "A";
         List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
-        
+
+        Stream<String> citiesStartingWithGivenLetter = cities.stream()
+                .filter(city -> city.startsWith(startingLetter));
+        System.out.println(citiesStartingWithGivenLetter.collect(Collectors.toList()));
     }
 }
---
+++ week-06/day-2/src/Exercise8.java
@@ -0,0 +1,6 @@
+public class Exercise8 {
+    public static void main(String[] args) {
+
+        //Write a Stream Expression to concatenate a Character list to a string!
+    }
+}
