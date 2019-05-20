14b01132181c76f1c3524885baf878f74850cc42
tmasner <magictonda@gmail.com>
2019-05-20 13:26:38 +0200

    pushed homeworks thymeleaf

---
+++ week-07/day-1/src/main/java/com/greenfox/anthony/banksimba/BanksimbaApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.anthony.banksimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksimbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanksimbaApplication.class, args);
+    }
+
+}
---
+++ week-07/day-1/src/main/java/com/greenfox/anthony/banksimba/model/BankAccount.java
@@ -0,0 +1,16 @@
+package com.greenfox.anthony.banksimba.model;
+
+public class BankAccount {
+
+    private String name;
+    private int balance;
+    private String animalType;
+
+    public BankAccount(String name, int balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
+
+    
+}
