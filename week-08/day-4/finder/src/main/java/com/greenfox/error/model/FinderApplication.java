8be321ea4bb6a6a29963ce9c1a5aa0aac521b467
tmasner <magictonda@gmail.com>
2019-05-30 16:17:12 +0200

    worked on error in stacktrace analyzer

---
+++ week-08/day-4/finder/src/main/java/com/greenfox/error/model/FinderApplication.java
@@ -0,0 +1,18 @@
+package com.greenfox.error.model;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderApplication.class, args);
+	}
+
+	@Override
+	public void run(String...run) throws Exception {
+
+	}
+}
---
+++ week-08/day-4/finder/src/main/java/com/greenfox/error/model/service/UserService.java
@@ -0,0 +1,28 @@
+package com.greenfox.error.model.service;
+
+import com.greenfox.error.model.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+
+    private ArrayList<User> users;
+
+    public UserService() {
+        this.users = new ArrayList<>();
+    }
+
+    public ArrayList<User> getAll() {
+        return users;
+    }
+
+    public void save(User user) {
+        users.add(user);
+    }
+
+    public UserService service() {
+        return new UserService();
+    }
+}
